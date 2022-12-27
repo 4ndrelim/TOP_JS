724711db1809aa432cd9b3963bbf80bc6e99e771
andre <tiltedaf3@gmail.com>
2022-12-27 16:34:53 +0800

    Add more linked list methods

--- assignments/linkedlist/LinkedList.js
+++ assignments/linkedlist/LinkedList.js
@@ -22,9 +22,50 @@ const LinkedList = () => {
         return HEAD.value;
     };
 
+    const prepend = (value) => {
+        const newNode = Node(value);
+        length++;
+        if (HEAD == null) {
+            return (HEAD = newNode);
+        }
+        newNode.nextNode = HEAD;
+        HEAD = newNode;
+    };
+
+    const size = () => {
+        if (!HEAD) {
+            return 0;
+        }
+        return length;
+    };
+
+    const tail = () => {
+        if (!HEAD) return null;
+        let pointer = HEAD;
+        while (pointer.nextNode !== null) {
+            pointer = pointer.nextNode;
+        }
+        return pointer.value;
+    };
+
+    const at = (index) => {
+        if (!HEAD) return null;
+        let pointer = HEAD;
+        for (let i = 0; i < index; i++) {
+            pointer = pointer.nextNode;
+        }
+        return pointer;
+    };
+
+    
+
     return {
         append,
-        head
+        head,
+        prepend,
+        size,
+        tail,
+        at,
     };
 };
 
--- assignments/linkedlist/LinkedList.test.js
+++ assignments/linkedlist/LinkedList.test.js
@@ -2,5 +2,11 @@
 import LinkedList from "./LinkedList.js";
 
 const list = LinkedList();
+console.log(list.size());
 list.append("node1");
 console.log(list.head());
+list.prepend("node0");
+console.log(list.head());
+list.append("node2");
+console.log(list.tail());
+console.log(list.at(1));
