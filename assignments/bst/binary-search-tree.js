07776ac1634c92e4830c8b5df6f913203d603437
andre <tiltedaf3@gmail.com>
2022-12-28 11:47:50 +0800

    Add buildtree function and test

--- assignments/bst/binary-search-tree.js
+++ assignments/bst/binary-search-tree.js
@@ -3,5 +3,63 @@ class Node {
         this.data = data;
         this.left = null;
         this.right = null;
-    }
+    };
 }
+
+class Tree {
+    constructor(array) {
+        // remove duplicates
+        this.array = [...new Set(array)].sort();
+        this.root = this.buildTree(0, this.array.length-1);
+    };
+
+    buildTree(start, end) {
+        
+        if (start > end) {
+            return null;
+        }
+        let mid = Math.floor((start + end)/2);
+        let root = new Node(this.array[mid]);
+
+        root.left = this.buildTree(start, mid-1);
+        root.right = this.buildTree(mid+1, end);
+
+        return root;
+    };
+
+    insert(value, root = this.root) {
+
+    };
+
+    traverse(root, array) {};
+
+    find(value, root = this.root) {};
+
+    delete(value, root = this.root) {};
+
+    levelOrder(root) {};
+
+    preOrder(root) {};
+
+    inOrder(root) {};
+
+    postOrder(root) {};
+
+    height(root) {};
+
+    depth(node, root = this.root) {};
+
+    prettyPrint(node = this.root, prefix = '', isLeft = true) {
+        if (node.right !== null) {
+            this.prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+          }
+          console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+          if (node.left !== null) {
+            this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+          }
+    };
+}
+
+tree = new Tree([4,7,3,2,1,2]);
+tree.prettyPrint();
+
