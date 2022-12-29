8f5942580510a9b9135ceb785538b22784f17c6b
andre <tiltedaf3@gmail.com>
2022-12-29 20:35:02 +0800

    basic layout

--- restaurant_page/src/website.js
+++ restaurant_page/src/website.js
@@ -1,6 +1,6 @@
 function createHeader() {
     const header = document.createElement("header");
-    header.classList.add("header");
+    header.classList.add("header"); // styling later on
     const restaurantName = document.createElement("h1");
     // missing stuff
     header.appendChild(restaurantName);
@@ -14,12 +14,42 @@ function createNav() {
     homeButton.classList.add("button-nav");
     homeButton.textContent = "Home";
     // missing event listener for loadHomePage
+
+    const menuButton = document.createElement("button");
+    menuButton.textContent = "Menu";
+    // missing event listener for loadMenu
+
+    const contactButton = document.createElement("button");
+    contactButton.textContent = "Contact";
+    // missing event listener for button
+
+    nav.appendChild(homeButton);
+    nav.appendChild(menuButton);
+    nav.appendChild(contactButton);
+    return nav;
+}
+
+function createMainBody() {
+    const mainBody = document.createElement("main");
+    // styling for main
+    mainBody.setAttribute("id", "mainBody");
+    return mainBody;
+}
+
+function createFooter() {
+    const footer = document.createElement("footer");
+    // styling for footer
+    const someText = document.createElement("p");
+    someText.textContent = "footer";
+    footer.appendChild(someText);
+    return footer;
 }
 
 function initializeWebsite() {
     const content = document.getElementById("content");
-    content.textContent = "hi";
     content.appendChild(createHeader());
+    content.appendChild(createMainBody());
+    content.appendChild(createFooter());
 }
 
 export default initializeWebsite;
